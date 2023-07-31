e7875e907a6ac44138762d2d9c01e465f4ad5fa1
Park Chan <qkrcks8028@gmail.com>
2023-07-31 15:32:49 +0900

    0731 연습

---
+++ typescript/index.js
@@ -0,0 +1,11 @@
+var a = 3;
+var b = "aldh dh";
+var c = 4;
+c = 'sdfsdf';
+var d = "dffdf";
+var e = ['apple', 'mango'];
+var f = [1, 2];
+function addNumber(a, b) {
+    return a + b;
+}
+addNumber(3, 6);
---
+++ typescript/index.ts
@@ -0,0 +1,15 @@
+let a:number = 3;
+let b:string = "aldh dh";
+
+let c:any = 4;
+c= 'sdfsdf';
+
+let d:number | string = "dffdf";
+
+let e:string[] = ['apple', 'mango'];
+let f:number[] = [1,2];
+
+function addNumber (a:number, b:number):number {
+    return a+b
+}
+addNumber(3,6);
